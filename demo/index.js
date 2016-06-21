e9c5eaf0f8693ba55a3106a02c769d65d63a3d08
Andrea Sonny <andreasonny83@gmail.com>
2016-06-21 21:24:37 +0100

    port fixed

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.argv[2] || 8888;
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/node_modules/responsive-navbar'));
