cefa8a909f9da0b134c7422af2e4a46c8bba4d02
Andrea SonnY <andreasonny83@gmail.com>
2016-06-27 08:57:38 +0100

    sw (#11)

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/node_modules/responsive-navbar'));
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
-app.listen(port, () =>
-  console.log('Example app listening on port ' + port));
+app.listen(port, function() {
+  console.log('Example app listening on port ' + port);
+});
